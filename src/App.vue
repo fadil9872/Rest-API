570f0111f60d6579adc8f18f56157e774c5d517e
fadil <adian281r@gmail.com>
2021-12-15 14:24:23 +0700

    composition api #5 computed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="">Count : {{ nilai }}</div>
   <button @click="add">Add</button>
+
+  <div class="">Result : {{ result }}</div>
+  
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import { reactive, ref, computed, toRefs } from 'vue';
 
 export default {
   setup() {
@@ -13,13 +16,18 @@ export default {
     });
 
     //method 
+    const addNum = ref(1);
+
     const add = () => {
       counter.nilai++;
     }
 
+    const result = computed(() => counter.nilai + addNum.value)
+
     return {
       ...toRefs(counter),
-      add
+      add,
+      result,
     }
   }
 }
