0e6da3ffe1cdbd3b5ebd386904d2d51e89415102
fadil <adian281r@gmail.com>
2021-12-15 12:01:41 +0700

    composition api #1 Ref

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">{{ message }}</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref("halo");
+
+    setTimeout(() => {
+      console.log(message);
+      message.value = "composition api";
+    }, 2000);
+
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
